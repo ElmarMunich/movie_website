ea3e913d5e7eca5633fbf2f86bf095d8cb31c1ae
Elmar Bayer <elmar.bayer@gmail.com>
2018-05-14 09:27:31 +0200

    comment added to function in media.py

--- media.py
+++ media.py
@@ -1,7 +1,8 @@
 import webbrowser
 class Movie():
-# defines class Movie         
+# defines class Movie
     def __init__ (self, movie_title, poster_image, trailer_youtube, movie_director):
+    # initializes the details for each movie with titel, artwork, trailer and director
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
